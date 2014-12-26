95171f8c9d35591d059583d83a02f134414003ee
agustinparmisano <agustinparmisano@gmail.com>
2014-12-26 14:53:39 -0300

    Solucionado la redireccion en node web kit pero hay q buscar una manera de rutear mejor, falta la pantalla del docente

--- js/login-module-ng.js
+++ js/login-module-ng.js
@@ -11,11 +11,18 @@ login.controller("loginButtonCtrl", function loginButton($scope) {
 	};
 
 	$scope.login = function(){
-		console.log($scope.name);
-		console.log($scope.pass);
+		//console.log($scope.name); Testing
+		//console.log($scope.pass); Testing
 		if (($scope.name == "docente") && ($scope.pass == "docente")){
 			console.log("ENTRA EL DOCENTE");
-			window.location.href="/docenteProblema.html";
+			//BUSCAR LA MEJOR MANERA DE HACER ROUTING
+			$currentPath = window.location;
+			$currentString = String($currentPath);
+			console.log("CURRENT STRING: " + $currentString);
+			$localPath = $currentString.slice(0,-10); //PARA NODE WEB KIT
+			$destinationPath = $localPath + "docenteProblema.html";
+			console.log("DESTINATION: " + $destinationPath);
+			window.location.href=$destinationPath;
 		};
 	}
 
