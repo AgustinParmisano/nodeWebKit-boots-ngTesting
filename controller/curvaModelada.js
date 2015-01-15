89040ccad6f18c4a4ec77962b486b03f765fcf32
Carla Luna Gennari <carla.lunagennari@hotmail.com>
2015-01-14 22:37:03 -0300

    agrego curva en la vista

---
+++ controller/curvaModelada.js
@@ -0,0 +1,49 @@
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'area'
+        },
+        title: {
+            text: 'Curva modelada'
+        },
+        subtitle: {
+            text: 'Taller de Prospeccion Geofisica'
+        },
+        xAxis: {
+            categories: ['10', '50', '80', '100', '130', '150', '200'],
+            tickmarkPlacement: 'on',
+            title: {
+                enabled: false
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Profundidad'
+            },
+            labels: {
+                formatter: function () {
+                    return this.value / 1000;
+                }
+            }
+        },
+        tooltip: {
+            shared: true,
+            valueSuffix: ' millions'
+        },
+        plotOptions: {
+            area: {
+                stacking: 'normal',
+                lineColor: '#666666',
+                lineWidth: 1,
+                marker: {
+                    lineWidth: 1,
+                    lineColor: '#666666'
+                }
+            }
+        },
+        series: [ {
+            name: 'Curva',
+            data: [2, 2, 20, 30, 13, 5, 1]
+        }]
+    });
+});
