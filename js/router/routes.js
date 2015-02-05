ffebd42020f1c89ebde1a767c362cf3040cffad3
agustinparmisano <agustinparmisano@gmail.com>
2015-02-05 19:21:25 -0300

    Agregado el modulo de ruteo de angular e intentando hacerlo andar de a poco, si anda el ruteo de angular anda node-webkit bien, mientras hay que seguir con lo del loader de tom, las variables, formulas y graficos

---
+++ js/router/routes.js
@@ -0,0 +1,10 @@
+/*login.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '/index.html',
+            controller: 'loginButtonCtrl'
+        }).when('/index.html/view/docenteProblema.html', {
+            templateUrl: '/view/docenteProblema.html',
+            controller: 'problemCtrl'
+        });
+});*/
