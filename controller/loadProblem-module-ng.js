8a1c7ffe14a643cbcf35aa6c5c5435ea0eba39f4
agustinparmisano <agustinparmisano@gmail.com>
2015-02-12 15:33:01 -0300

    Pantalla de cargado de problema casi terminada, faltan los problemas por defecto y seguir con el boton continuar para la siguiente pantalla. Ojo que el controlador de la siguiente pantalla va en el mismo modulo que este controlador de la pantalla de cargado para poder hacer el pasaje de parametros correctamente desde los archivos.

--- controller/loadProblem-module-ng.js
+++ controller/loadProblem-module-ng.js
@@ -4,11 +4,9 @@
 * o elegir un problema por default
 */
 var loadProblem = angular.module("loadProblemModule", []);
-var experiment = angular.module("experimentModule", ["loadProblemModule"]);
-
 
 loadProblem.controller('loadProblemCtrl', ['$scope', function($scope) {
-	
+
 	//Cargar el problema por default
 	$scope.cargarDefault = function() {
 		/*Esto lo uso para probar y entrar a la vista del alumno pero se podria cargar un datos por defaut*/
@@ -28,7 +26,9 @@ loadProblem.controller('loadProblemCtrl', ['$scope', function($scope) {
         //$scope.content = $fileContent;
         this.problema = $fileContent;
         console.log("JSON: " + this.problema);
-        alert($fileContent);
+        parsed = JSON.parse($fileContent);
+        $scope.enunciado = parsed.enunciado;
+        console.log("enun: " + $scope.enunciado);
     };
 
 }]);
@@ -60,127 +60,3 @@ loadProblem.service('problemaJson', function () {
 	var problema = this.problema;
 
 });
-
-loadProblem.controller('experimentCtrl', ['$scope', 'problemaJson', function($scope, problemaJson) {
-	$isWritten = false;
-	$isLong = false;
-	$tooLong = false;
-	
-	$scope.enunciado="Esto es el enunciado";
-	$scope.dmax=1000;
-	$scope.costo=0;
-	console.log(JSON.stringify(problemaJson));
-
-	$scope.change = function() {
-	
-		console.log(this.xfin);	
-
-		this.xfinErrorMsg = "";
-
-		$isWritten = true;
-		ctx.font = "20px Arial";
-
-		if(this.xfin > 9999){
-			$isLong = true;
-		}
-
-		if (this.xfin == null) {
-			this.xfin = "";
-		};
-
-		if (isNaN(this.xfin)) {
-			this.xfinErrorMsg = "Ingrese Numeros";
-		}else if(this.xfin > 999999){
-			$bigNum = this.xfin;
-			this.xfinErrorMsg = $bigNum + " muy grande"
-			this.xfin = $bigNum;
-		}
-
-		if($isLong){
-			ctx.fillText(this.xfin,420,80);
-		}else{
-			ctx.fillText(this.xfin,440,80);
-		}
-		if (this.xini != "" && this.xini>this.xfin) {
-			this.xiniErrorMsg = "X inicial debe ser menor a X final"
-		}else{
-			this.xiniErrorMsg = ""
-		};
-
-		if ($isWritten) {
-			if($isLong){
-				ctx.clearRect(400, 60, 420, 80);
-				$isLong = false;
-				$isWritten = false;
-				ctx.fillText(this.xfin,420,80);
-			}else{
-				ctx.clearRect(420, 60, 440, 80);
-				$isWritten = false;
-				ctx.fillText(this.xfin,440,80);
-			}
-		};
-	};
-	$scope.changeIni = function() {
-	
-		console.log(this.xini);		
-
-		this.xiniErrorMsg = "";
-
-		$isWritten = true;
-		ctx.font = "20px Arial";
-
-		if(this.xini > 9999){
-			$isLong = true;
-		}
-
-		if (this.xini == null) {
-			this.xini = "0";
-		};
-
-		if (isNaN(this.xini)) {
-			this.xiniErrorMsg = "Ingrese Numeros";
-		}else if(this.xini > 999999){
-			$bigNum = this.xini;
-			this.xiniErrorMsg = $bigNum + " muy grande"
-			this.xini = $bigNum;
-		}
-		if ($isWritten) {
-			if($isLong){
-				ctx.clearRect(20, 60, 440, 80);
-				$isLong = false;
-				$isWritten = false;
-				ctx.fillText(this.xini,20,80);
-			}else{
-				ctx.clearRect(20, 60, 440, 80);
-				$isWritten = false;
-				ctx.fillText(this.xini,20,80);
-			}
-		
-		};
-		if (this.xfin != "" && this.xini>this.xfin) {
-			this.xiniErrorMsg = "X inicial debe ser menor a X final"
-		}else{
-			this.xiniErrorMsg = ""
-		};
-	};
-	
-	$scope.calcularPasos = function(){
-		console.log("calcularPasos tira error en el html");
-	}
-	
-	/* Despues de establecer el experimento hay que pasar a la vista del problema, 
-	habiendo validado previamente los datos correspondientes*/
-	$scope.continuar= function(){
-		/*Validar y enviar datos*/
-	
-		console.log("Pasar a experimento alumno");
-		$currentPath = window.location;
-		$currentString = String($currentPath);
-		console.log("CURRENT STRING: " + $currentString);
-		$localPath = $currentString;//.slice(0,-10); //PARA NODE WEB KIT
-		//esto hay que arreglarlo
-		$destinationPath = "http://localhost:8080/view/alumnoModelado";
-		console.log("DESTINATION: " + $destinationPath);
-		window.location.href=$destinationPath;
-	};
-}]);
