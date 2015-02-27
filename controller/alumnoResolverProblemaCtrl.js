43945c285dd732bf728e044a0abda4095c630a1e
agustinparmisano <agustinparmisano@gmail.com>
2015-02-27 11:39:19 -0300

    Falta la funcion que grafica la curva, podemos sacarla de la otra que ya está hecha, ya dejé la función declarado con el json pasado por parámetro al final del controller.js del alumno

--- controller/alumnoResolverProblemaCtrl.js
+++ controller/alumnoResolverProblemaCtrl.js
@@ -155,86 +155,12 @@ app.controller('canvasCtrl', ['$scope',  function($scope) {
 				$scope.costoTotal=$scope.nPasos * $scope.problema.costoMedicion;
 				console.log($scope.costoTotal + "Hay q ponerlo en la variable");
 			}
-	        //inicializar mensajes de error en nada:
-	        //this.dmaxErrorMsg = "";
-	        
-	        /*if (isNaN(this.dmax) || this.dmax == null) {
-	            this.dmaxErrorMsg = "Ingrese Numeros";
-	            setupCanvas();
-	            noGraficar = true;
-	        }else if (this.dmax > 1000) {
-	            this.dmaxErrorMsg = "Debe ser menor a 1000";
-	            noGraficar = true;
-	        };
-			if (isNaN(this.z)) {
-				this.z0ErrorMsg = "Ingrese Numeros";
-	            noGraficar = true;
-			}else if (this.z < 75) {
-	            this.z0ErrorMsg = "Debe ser mayor a 75";
-	            noGraficar = true;
-	        };
-			if (isNaN(this.r)) {
-				this.r0ErrorMsg = "Ingrese Numeros";
-			}
-			if (isNaN(this.x)) {
-				this.x0ErrorMsg = "Ingrese Numeros";
-			}
+            drawChart($scope.problema);
 
-	        if (isNaN(this.di)) {
-	            this.diErrorMsg = "Ingrese Numeros";
-	        }else if (this.di > 15) {
-	            this.diErrorMsg = "Debe ser menor a 15";
-	            noGraficar = true;
-	        };
-	        if (isNaN(this.de)) {
-	            this.deErrorMsg = "Ingrese Numeros";
-	        }else if (this.de > 15) {
-	            this.deErrorMsg = "Debe ser menor a 15";
-	            noGraficar = true;
-	        };
-
-	        if ((this.x + this.r) > this.dmax) {
-	            this.x0ErrorMsg = "X + R debe ser menor a L";
-	            this.r0ErrorMsg = "X + R debe ser menor a L";
-	            this.dmaxErrorMsg = "X + R debe ser menor a L";
-	            noGraficar = true;
-	        };
-
-	        if (this.x > this.dmax) {
-	            this.x0ErrorMsg = "X mas grande que dmax";
-	            noGraficar = true;
-	        }
-
-	        if(this.z > 300){
-	            //NO ANDA EL MENSAJE DE ERROR
-	            this.z0ErrorMsg = "Z debe ser menor a 300."
-	        }
-
-	        if(this.dmax < 50){
-	            //NO ANDA EL MENSAJE DE ERROR
-	            this.dmaxErrorMsg = "L debe ser mayor a 50."
-	        }
-
-
-			if(!noGraficar && !(isNaN(this.dmax)) && this.z != null && this.r != null && this.x != null  && this.di != null  && this.de != null && this.x < this.dmax && this.z < 300 && this.dmax > 50){
-	            var ejeX = this.x + 60;
-	            var ejeZ = this.z;
-	            var valorFormula1 = 0;
-	            var valorFormula2 = 0;
-	            var valorFormula3 = 0;
-
-	            ejeX = Math.round(490 / this.dmax * this.x + 60);
-	            //$scope.x = ejeX;
-
-	            console.log("X " + ejeX);
-	            console.log("Z " + ejeZ);
-
-	            ctx.moveTo(ejeX,ejeZ);
-				ctx.arc(ejeX,ejeZ,this.r,30,(Math.PI/180)*360,true);
-				ctx.fillStyle="#000000";
-				ctx.fill();
-	            drawChart($scope);
-				
-			}*/
 		};
 }]);
+
+function drawChart(problema){
+	//Funcion que grafica la curva mediante parametrojson problema
+	console.log(problema);
+};
