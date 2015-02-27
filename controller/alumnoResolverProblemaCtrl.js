d971f857961283db7c8010b0ed1e1f4b2e754b58
agustinparmisano <agustinparmisano@gmail.com>
2015-02-26 21:22:40 -0300

    Haciendo la vista de resolucion de problema del alumno, canvas terminado e inputs terminados

--- controller/alumnoResolverProblemaCtrl.js
+++ controller/alumnoResolverProblemaCtrl.js
@@ -1,5 +1,5 @@
 
- app.controller('alumnoResolverProblemaCtrl', ['$scope', '$location', '$routeParams', function($scope, $location, $routeParams) {
+app.controller('alumnoResolverProblemaCtrl', ['$scope', '$location', '$routeParams', function($scope, $location, $routeParams) {
 
 	
 	$scope.problema=$routeParams.problema.substring(9,$routeParams.problema.length);
@@ -7,6 +7,8 @@
 	var parsed=JSON.parse($scope.problema);
 	$scope.problema=parsed;
 	$scope.enunciado=$scope.problema.enunciado;
+	$scope.dmax=parseInt($scope.problema.dmax);
+	$scope.dd=parseInt($scope.problema.dd);
 	//var p=angular.fromJSON(problema);
 	//alert(p);
 		
@@ -22,6 +24,8 @@ app.controller('inputsCtrl', ['$scope', function($scope){
 		xInicial = $scope.xini;
 		if (xFinal != "" && xFinal < xInicial) {
 			this.xiniErrorMsg = "Inicial debe ser menor a final"
+		}else if (xInicial > $scope.dmax) {
+			this.xiniErrorMsg = "Debe ser menor a la longitud"
 		}else{
 			this.xfinErrorMsg = "";
 			this.xiniErrorMsg = "";
@@ -29,11 +33,12 @@ app.controller('inputsCtrl', ['$scope', function($scope){
 	};
 
 	$scope.changeFin = function(){
-		console.log("AAAAAAA");
 		this.xfinErrorMsg = "";
 		xFinal = $scope.xfin;
 		if (xInicial != "" && xFinal < xInicial) {
 			this.xiniErrorMsg = "Final debe ser mayor a inicial"
+		}else if (xFinal > $scope.dmax) {
+			this.xfinErrorMsg = "Debe ser menor a la longitud"
 		}else{
 			this.xfinErrorMsg = "";
 			this.xiniErrorMsg = "";
@@ -43,15 +48,17 @@ app.controller('inputsCtrl', ['$scope', function($scope){
 	$scope.calcularPasos = function(){
 		xInicial = $scope.xini;
 		xFinal = $scope.xfin;
-		console.log("INI " + xInicial + " FIN " + xFinal);
+		//Falta la formula de los costos
+		console.log("Formula de costo INI: " + xInicial + " FIN: " + xFinal);
 	};
 
 }]);
 
-function setupCanvas(){
+function setupCanvas(dmax){
 
     var oldcanv = document.getElementById('graficoDocente');
     var canvDiv = document.getElementById('canvasDiv');
+
     canvDiv.removeChild(oldcanv);
 
     var canv = document.createElement('canvas');
@@ -64,21 +71,80 @@ function setupCanvas(){
 
     c = document.getElementById("graficoDocente");
     ctx = c.getContext("2d");
-    crearLinea();
+    crearLinea(dmax);
 
 }
 
+function crearLinea(dmax){
+    /*Esto grafica la recta*/
+    ctx.moveTo(50,50);
+    ctx.lineTo(550,50);
+    ctx.moveTo(50,40);
+    ctx.lineTo(50,60);
+    ctx.font = "20px Arial";
+    ctx.fillText("0",35,80);
+    ctx.fillText(dmax,550,80);
+    ctx.moveTo(550,40);
+    ctx.lineTo(550,60);
+    ctx.stroke();
+    ctx.fill();
+    ctx.fillText("",520,80);
+};
+
+function graficarPorcion(ini, fin, scope){
+	var finPos = Math.round(490 / scope.dmax * fin + 60)
+	var iniPos = Math.round(490 / scope.dmax * ini + 60)
+
+    ctx.moveTo(50,50);
+    ctx.lineTo(550,50);
+    ctx.moveTo(iniPos,40);
+    ctx.lineTo(iniPos,60);
+    ctx.font = "20px Arial";
+    ctx.fillText(ini,iniPos,80);
+    ctx.moveTo(finPos,40);
+    ctx.lineTo(finPos,60);
+    ctx.fillText(fin,finPos,80);
+    ctx.stroke();
+    ctx.fill();
+    ctx.fillText("",520,80);
+};
+
 app.controller('canvasCtrl', ['$scope',  function($scope) {
+		setupCanvas($scope.dmax);
 
 		$scope.graficar= function(){
 	        var noGraficar = false;
-	        setupCanvas();
-	        ctx.fillText(this.dmax,520,80);
-	        
+	        setupCanvas($scope.dmax);
+	        this.graficErrorMsg = "";
+
+	        if(this.xfin == null){
+	        	this.graficErrorMsg = "Complete Fin"
+	        	noGraficar = true;
+	        }
+
+	        if(this.xini == null){
+	        	this.graficErrorMsg = "Complete Inicio"
+	        	noGraficar = true;
+	        }
+
+
+	        if(this.xfin > $scope.dmax){
+	        	this.graficErrorMsg = "Fin debe ser menor a la longitud"
+	        	noGraficar = true;
+	        }
+
+	        if(this.xfin < this.xini){
+	        	this.graficErrorMsg = "Inicio debe ser menor a fin"
+	        	noGraficar = true;
+	        }
+
+	        if(!noGraficar){
+	        	graficarPorcion(this.xini, this.xfin, $scope);
+	        }
 	        //inicializar mensajes de error en nada:
 	        //this.dmaxErrorMsg = "";
 	        
-	        if (isNaN(this.dmax) || this.dmax == null) {
+	        /*if (isNaN(this.dmax) || this.dmax == null) {
 	            this.dmaxErrorMsg = "Ingrese Numeros";
 	            setupCanvas();
 	            noGraficar = true;
@@ -154,8 +220,7 @@ app.controller('canvasCtrl', ['$scope',  function($scope) {
 				ctx.fillStyle="#000000";
 				ctx.fill();
 	            drawChart($scope);
-
-			}
+				
+			}*/
 		};
-
 }]);
